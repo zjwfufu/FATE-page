841a87d1614aa7046a2bc4e820b375cba545eea5
zjwXDU <1174987765@qq.com>
2024-11-24 00:13:33 +0800

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,88 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+var INTERP_BASE = "https://homes.cs.washington.edu/~kpar/nerfies/interpolation/stacked";
+var NUM_INTERP_FRAMES = 240;
+
+var interp_images = [];
+function preloadInterpolationImages() {
+  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
+    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
+    interp_images[i] = new Image();
+    interp_images[i].src = path;
+  }
+}
+
+function setInterpolationImage(i) {
+  var image = interp_images[i];
+  image.ondragstart = function() { return false; };
+  image.oncontextmenu = function() { return false; };
+  $('#interpolation-image-wrapper').empty().append(image);
+}
+
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function() {
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    var optionsThree = {
+        slidesToScroll: 1,
+        slidesToShow: 3,
+        loop: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+    };
+
+    var optionsOne = {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: false,
+        autoplaySpeed: 3000,
+    };
+
+    // Initialize the carousel showing 3 videos at a time
+    var carouselThree = bulmaCarousel.attach('#carousel-three', optionsThree);
+
+    // Initialize the carousel showing 1 video at a time
+    var carouselOne = bulmaCarousel.attach('#carousel-one', optionsOne);
+
+    // Loop on each carousel initialized
+    carouselThree.forEach(function(carousel) {
+        carousel.on('before:show', state => {
+            console.log(state);
+        });
+    });
+
+    carouselOne.forEach(function(carousel) {
+        carousel.on('before:show', state => {
+            console.log(state);
+        });
+    });
+
+    // Access to bulmaCarousel instance of an element
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+        // bulmaCarousel instance is available as element.bulmaCarousel
+        element.bulmaCarousel.on('before-show', function(state) {
+            console.log(state);
+        });
+    }
+
+    preloadInterpolationImages();
+
+    $('#interpolation-slider').on('input', function(event) {
+        setInterpolationImage(this.value);
+    });
+    setInterpolationImage(0);
+    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
+
+    bulmaSlider.attach();
+});
+
